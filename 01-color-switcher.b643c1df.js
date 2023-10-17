8e6dffad716661c331447584ef294a9feeb25487
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-17 10:54:59 +0000

    Deploying to gh-pages from @ uflakey/goit-js-hw-09@7a1c3ba00a0c87645da468db97844ce8e5452327 🚀

---
+++ 01-color-switcher.b643c1df.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;t.addEventListener("click",(function(a){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(a){t.disabled=!1,e.disabled=!0,clearInterval(d)})),e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.b643c1df.js.map
